33e3106d8fc99e3ef986231c9b7f611947eec0da
BraydenRobinson <116311842+BraydenRobinson@users.noreply.github.com>
2022-11-29 14:55:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -114,6 +114,49 @@ def ip_selection_screen(screen: pygame.Surface):
 
 def battle(screen: pygame.Surface, selection: str, competitior_selection: str):
 	# Win
+	#
+	#
+	#
+	#
+	#
+	# Code added by Brayden (Start)
+	from tkinter import *
+	import time
+
+	WIDTH = 1000
+	HEIGHT = 1000
+	xVelocity = 4
+	yVelocity = 0
+	window = Tk()
+
+	canvas = Canvas(window,width=WIDTH,height=HEIGHT)
+	canvas.pack()
+
+	photo_image = PhotoImage(file='paper.png')
+	my_image = canvas.create_image(0,300,image=photo_image,anchor=W)
+
+	image_width = photo_image.width()
+	image_height = photo_image.height()
+
+	while True:
+    	coordinates = canvas.coords(my_image)
+    	print(coordinates)
+    	if(coordinates[0]>=(WIDTH-image_width) or coordinates[0]<0):
+        	xVelocity = -xVelocity
+    	if (coordinates[1]>=(HEIGHT-image_height) or coordinates[1]<0):
+        	yVelocity = -yVelocity
+    	canvas.move(my_image,xVelocity,yVelocity)
+    	window.update()
+    	time.sleep(0.01)
+
+	window.mainloop()
+	# Code added by Brayden (End)
+	#
+	#
+	#
+	#
+	#
+	
 	if items[selection].defeats(competitior_selection) and not items[
 		competitior_selection
 	].defeats(selection):
